142328aefe6fad008795c18f17fdac79dd5ffaf2
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-09 16:05:38 +0900

    fix(eslint): correct ESLint config and set Storybook rules
    Encounted to `ConfigError: Config (unnamed): Unexpected key "0" found.`
    error.
    
    This error fixed it to ESLint config updated.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,26 @@
 import config from '@kubosho/configs/eslint';
+import { defineConfig } from 'eslint/config';
+import storybook from 'eslint-plugin-storybook';
 
-export default {
+export default defineConfig([
   ...config,
-};
+  {
+    files: ['**/*.stories.ts', '**/*.stories.tsx'],
+    extends: [storybook.configs['flat/recommended']],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/.storybook/**/*.ts',
+            '**/*.config.*',
+            '**/*.test.*',
+          ],
+        },
+      ],
+    },
+  },
+]);
